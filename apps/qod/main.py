590545d2e41deb8fda1d59ac942973d336c3a4ed
woshishabii <51900038+woshishabii@users.noreply.github.com>
2023-02-18 17:49:34 +0800

    DB

--- apps/qod/main.py
+++ apps/qod/main.py
@@ -2,6 +2,9 @@ import os
 
 import requests
 
+import sqlalchemy
+import sqlalchemy.ext.declarative
+
 
 PEXELS_HEADERS = {
     'Authorization': os.getenv('PEXELS_API_KEY')
@@ -12,6 +15,26 @@ PEXELS_PARA = {
     'orientation': 'landscape'
 }
 
+PEXELS_QUERY = ['nature', 'sunset', 'sea']
+
+
+engine = sqlalchemy.engine.create_engine('sqlite:///./apps/qod/db.db?check_same_thread=False', echo=True)
+Base = sqlalchemy.ext.declarative.declarative_base()
+
+
+class Backgrounds(Base):
+    __tablename__ = 'backgrounds'
+    ID = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    pexels_id = sqlalchemy.Column(sqlalchemy.Integer)
+    url = sqlalchemy.Column(sqlalchemy.String(200))
+    avg_color = sqlalchemy.Column(sqlalchemy.String(7))
+    src = sqlalchemy.Column(sqlalchemy.String(200))
+    alt = sqlalchemy.Column(sqlalchemy.String(200))
+
+
+Base.metadata.create_all(engine, checkfirst=True)
+
 
 # Fetch Pexels Images
-print(requests.get('https://api.pexels.com/v1/search', params=PEXELS_PARA).json())
+# print(requests.get('https://api.pexels.com/v1/search', headers=PEXELS_HEADERS, params=PEXELS_PARA).json())
+
